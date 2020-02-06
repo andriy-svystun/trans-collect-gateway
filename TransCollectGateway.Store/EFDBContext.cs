6e79e5f05cb8ef459ff9da3fc7f7d92e135caa60
Andriy <andriy_s@meta.ua>
2020-02-06 12:01:42 +0200

    Add TransCollectGateway.Store DBContext

---
+++ TransCollectGateway.Store/EFDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using TransCollectGateway.Common;
+
+namespace TransCollectGateway.Store
+{
+    public class EFDBContext : DbContext
+    {
+        public EFDBContext() : base("TCGDatabase")
+        {
+            Database.SetInitializer(new MainContextInitializer());
+        }
+        DbSet<Transaction> Transactions { get; set; }
+    }
+
+    class MainContextInitializer : CreateDatabaseIfNotExists<EFDBContext>
+    {
+
+    }
+}
