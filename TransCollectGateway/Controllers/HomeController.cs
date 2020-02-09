ad724942f8316617b421455ad2a3f00a0388dd2f
Andriy <andriy_s@meta.ua>
2020-02-09 22:06:29 +0200

    Add Home controller. Add Layout page and referenced libraries

---
+++ TransCollectGateway/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TransCollectGateway.Common;
+
+namespace TransCollectGateway.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransLogUploadService _uploadService;
+
+        public HomeController(ITransLogUploadService uploadService)
+        {
+            _uploadService = uploadService;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
