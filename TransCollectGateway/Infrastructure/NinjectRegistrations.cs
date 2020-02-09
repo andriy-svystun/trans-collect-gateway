37c60071322b7ef7eae38d9172e460373de6e27d
Andriy <andriy_s@meta.ua>
2020-02-09 21:59:44 +0200

    Add Web project. Add Ninject IoC container

---
+++ TransCollectGateway/Infrastructure/NinjectRegistrations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+using TransCollectGateway.Common;
+using TransCollectGateway.Store;
+using TransCollectGateway.Infrastructure;
+
+namespace TransCollectGateway.Infrastructure
+{
+    public class NinjectRegistrations : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRepository<Transaction>>().To<TransactionRepository>();
+            Bind<ITransLogFileProcessing>().To<TransLogFileProcessing>();
+            Bind<ITransLogUploadService>().To<TransLogUploadService>();
+        }
+    }
+}
